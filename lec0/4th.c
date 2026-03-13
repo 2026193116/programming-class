7baa1e425145bffd955356b324704b3ccaf0dba1
2026193116 <flora20070105@gmail.com>
2026-03-13 04:21:32 +0000

    0313commit3

--- lec0/4th.c
+++ lec0/4th.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World\n");
-    return 0;
+    int a = 48;
+    printf("a %%d : %d\n", a);
+    printf("a %%i : %i\n", a);
+    printf("a %%c : %c\n", a);
+    printf("a %%f : %f\n", a);
+
+    return 0; 
 }
