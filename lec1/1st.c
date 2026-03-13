63ccfcbae1b53d07ccb6f3bdd724ca06fe1a916f
2026193116 <flora20070105@gmail.com>
2026-03-13 05:21:29 +0000

    send

--- lec1/1st.c
+++ lec1/1st.c
@@ -6,5 +6,32 @@ int main() {
     printf("a %%i : %i\n", a);
     printf("a %%c : %c\n", a);
     printf("a %%f : %f\n", (float)a);
+
+    char b = 'A';
+    printf("b %%d : %d\n", b);
+    printf("b %%i : %i\n", b);
+    printf("b %%c : %c\n", b+32);
+    printf("b %%f : %f\n", (float)b);
+
+    int c = -3;
+    printf("c %%d : %d\n", c);
+    printf("c %%i : %i\n", c);
+    printf("c %%c : %c\n", c);
+    printf("c %%f : %f\n", (float)c);
+    printf("c %%u : %u\n", c);
+
+    int d = 128;
+    printf("d %%d : %d\n", d);
+    printf("d %%i : %i\n", d);
+    printf("d %%c : %c\n", d);
+    printf("d %%f : %f\n", (float)d);
+    printf("d %%e : %e\n", d);
+
+    unsigned char ch=  254;
+    printf("ch %%d : %d\n", ch);
+    printf("ch %%i : %i\n", ch);
+    printf("ch %%c : %c\n", ch);
+    printf("ch %%f : %f\n", ch);
+    
     return 0; 
 }
